6e8081ae457c945bb48c396fe50c9a1eaa47de80
Daniel <daniel@heronode.io>
2018-11-19 14:20:12 +0800

    [HeroWallet] Sign with FaceID&TouchID

--- hero-ios/Classes/HeroColdWallet/HeroSignView.m
+++ hero-ios/Classes/HeroColdWallet/HeroSignView.m
@@ -6,20 +6,25 @@
 //
 
 #import "HeroSignView.h"
-#import "UIView+hero.h"
+#import "UIView+Hero.h"
 #import "UIView+Addition.h"
 #import "NSString+Hex.h"
 #import "NSData+HexString.h"
+#import "HeroBioMeteics.h"
+
+@interface HeroSignView ()
+@property (nonatomic) UITextField *passwordTextField;
+
+@property (nonatomic) UIImageView *bioImageView;
+@property (nonatomic) Transaction *tran;
+@end
 
 @implementation HeroSignView {
-    Transaction *_tran;
-    BOOL isFinger;
-    UITextField *_passwordTextField;
+    
 }
 
 - (instancetype)initWithTransaction:(Transaction *)tran {
     if (self = [super init]) {
-        isFinger = NO;
         _tran = tran;
         
         [self setupUI];
@@ -115,11 +120,67 @@
     inputTextView.layer.borderColor = UIColorFromRGB(0x979797).CGColor;
     inputTextView.layer.borderWidth = 1;
     inputTextView.textColor = UIColorFromRGB(0xff6666);
-    inputTextView.backgroundColor = UIColorFromRGB(0xfff0f0f0);
+    inputTextView.backgroundColor = UIColorFromRGB(0xf0f0f0);
     inputTextView.font = [UIFont systemFontOfSize:14];
-    inputTextView.frame = CGRectMake(40, 280, SCREEN_W-80, 110);
+    inputTextView.frame = CGRectMake(30, 280, SCREEN_W-30*2, 110);
     
-    if (isFinger) {
+    __weak HeroSignView *weakSelf = self;
+    void (^bioAuthUI)(HeroBioType type) = ^(HeroBioType type){
+        UILabel *confirmLabel = [UILabel new];
+        confirmLabel.text = @"密钥认证";
+        confirmLabel.font = [UIFont systemFontOfSize:14];
+        [contentView addSubview:confirmLabel];
+        confirmLabel.frame = CGRectMake(35, 413, 58, 17);
+        
+        UITextField *passwordTextField = [[UITextField alloc] initWithFrame:CGRectMake(35, 452, 180, 35)];
+        passwordTextField.borderStyle = UITextBorderStyleNone;
+        passwordTextField.layer.borderColor = UIColorFromRGB(0xafafaf).CGColor;
+        passwordTextField.layer.borderWidth = 1;
+        passwordTextField.placeholder = @"输入密码";
+        passwordTextField.secureTextEntry = YES;
+        [contentView addSubview:passwordTextField];
+        weakSelf.passwordTextField = passwordTextField;
+        
+        UIButton *confirmButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [contentView addSubview:confirmButton];
+        [confirmButton setTitle:@"确认" forState:UIControlStateNormal];
+        [confirmButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        confirmButton.backgroundColor = self.tintColor;
+        confirmButton.frame =  CGRectMake(35, 509, 180, 35);
+        [confirmButton addTarget:self action:@selector(sign) forControlEvents:UIControlEventTouchUpInside];
+        
+        UIView *line = [UIView new];
+        line.backgroundColor = UIColorFromRGB(0x979797);
+        [contentView addSubview:line];
+        line.frame = CGRectMake(confirmButton.right + 35, confirmLabel.top, 1, 140);
+        
+        NSBundle *bundle = [NSBundle bundleWithURL:[[NSBundle bundleForClass:[self class]] URLForResource:@"hero-ios" withExtension:@"bundle"]];
+        NSString *bioImageName = type == HeroBioFaceID ? @"faceID_s" : @"finger_s";
+        UIImage *logo = [UIImage imageNamed:bioImageName inBundle:bundle compatibleWithTraitCollection:nil];
+        weakSelf.bioImageView = [[UIImageView alloc] initWithImage:logo];
+        weakSelf.bioImageView.frame = CGRectMake(line.right+35, line.top, 100, 100);
+        [contentView addSubview:weakSelf.bioImageView];
+        weakSelf.bioImageView.userInteractionEnabled = YES;
+        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(bioTapped)];
+        [weakSelf.bioImageView addGestureRecognizer:tap];
+        
+        UILabel *bioLabel = [UILabel new];
+        bioLabel.text = type == HeroBioFaceID ? @"点击进入面部识别" : @"点击进入指纹识别";
+        bioLabel.textColor = UIColorFromRGB(0x6d6d6d);
+        bioLabel.frame = CGRectMake(0, 0, weakSelf.bioImageView.width, 18);
+        bioLabel.textAlignment = NSTextAlignmentCenter;
+        bioLabel.top = weakSelf.bioImageView.bottom+12;
+        bioLabel.centerX = weakSelf.bioImageView.centerX;
+        bioLabel.font = [UIFont systemFontOfSize:12];
+        [contentView addSubview:bioLabel];
+        
+    };
+    
+    if ([HeroBioMeteics type] == HeroBioTouchID) {
+        bioAuthUI(HeroBioTouchID);
+        
+    } else if ([HeroBioMeteics type] == HeroBioFaceID) {
+        bioAuthUI(HeroBioFaceID);
         
     } else {
         UILabel *confirmLabel = [UILabel new];
@@ -170,15 +231,31 @@
     [self removeFromSuperview];
 }
 
+- (void)bioTapped {
+    [HeroBioMeteics evaluate:@"签名" reply:^(BOOL success, NSError * _Nonnull error) {
+        if (success) {
+            if (self.done) {
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    self.done([[[HeroWallet sharedInstance] defaultAccount] signTx:self.tran]);
+                    [self removeFromSuperview];
+                });
+            }
+        }
+    }];
+}
+
 - (void)sign {
     if ([_passwordTextField.text isEqualToString:[[HeroWallet sharedInstance] defaultAccount].password]) {
         if (self.done) {
             self.done([[[HeroWallet sharedInstance] defaultAccount] signTx:_tran]);
-            [self removeFromSuperview];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self removeFromSuperview];
+            });
         }
     } else {
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"错误" message:@"密码不正确" delegate:nil cancelButtonTitle:@"确认" otherButtonTitles:nil];
         [alert show];
     }
 }
+
 @end
