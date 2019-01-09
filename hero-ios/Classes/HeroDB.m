3efb35e2970511f2cc91f81f86e1df1ec42c1585
Daniel <daniel@heronode.io>
2019-01-09 11:05:01 +0800

    add herodb module

--- hero-ios/Classes/HeroDB.m
+++ hero-ios/Classes/HeroDB.m
@@ -6,6 +6,7 @@
 //
 
 #import <JavaScriptCore/JavaScriptCore.h>
+#import <Objective_LevelDB/LevelDB.h>
 #import "HeroDB.h"
 
 
@@ -14,35 +15,81 @@
 @end
 
 @implementation HeroDB {
-    id ldb;
+    LevelDB *ldb;
 }
 -(instancetype)init{
-//    LevelDB *ldb = [LevelDB databaseInLibraryWithName:@"test.ldb"];
+    ldb = [LevelDB databaseInLibraryWithName:@"hero.ldb"];
     return [super init];
 }
 -(void)on:(NSDictionary *)json{
-    if (json[@"isNpc"]) {
-        NSString *key = json[@"key"];
-        id value = json[@"value"];
+    NSString *key = json[@"key"];
+    NSString *arrayKey = json[@"arrayKey"];
+    NSString *start = json[@"start"];
+    NSString *count = json[@"count"];
+    id value = json[@"value"];
+    
+    if (key) {
         if (value) {
-            ldb[key] = value;
-        }else{
-            NSString *js = [NSString stringWithFormat:@"window['%@callback'](%@)",[self class],[[NSString alloc] initWithData:ldb[key] encoding:NSUTF8StringEncoding]];
-            [self.controller.webview stringByEvaluatingJavaScriptFromString:js];
-        }
-        NSString *arrayKey = json[@"arrayKey"];
-        NSString *start = json[@"start"];
-        NSString *end = json[@"end"];
-        if (arrayKey) {
-//            [ldb enumerateKeysAndObjectsUsingBlock:^(LevelDBKey *key, id value, BOOL *stop) {
-//                // This step is necessary since the key could be a string or raw data (use NSDataFromLevelDBKey in that case)
-//                NSString *keyString = NSStringFromLevelDBKey(key); // Assumes UTF-8 encoding
-//                // Do something clever
-//            }];
-            if (value) {
-//                
+            [self setValue:value forKey:key];
+        } else{
+            id value = [self valueForKey:key];
+            if (json[@"isNpc"]) {
+                NSString *js = [NSString stringWithFormat:@"window['%@callback'](%@)",[self class], value];
+                [self.controller.webview stringByEvaluatingJavaScriptFromString:js];
+            } else {
+                [self.controller on:@{@"result": value}];
             }
         }
+    }
+    
+    if (arrayKey) {
+        if (value) {
+            [self addValue:value forArrayKey:arrayKey];
+        } else if (start && count) {
+            NSArray *value = [self valueForArrayKey:arrayKey start:[start integerValue] count:[count integerValue]];
+            
+            if (json[@"isNpc"]) {
+                NSData *jsonData = [NSJSONSerialization dataWithJSONObject:value options:NSJSONWritingPrettyPrinted error:nil];
+                NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+                NSString *js = [NSString stringWithFormat:@"window['%@callback'](%@)",[self class], jsonString];
+                [self.controller.webview stringByEvaluatingJavaScriptFromString:js];
+            } else {
+                [self.controller on:@{@"result": value}];
+            }
+        
+        }
+    }
+}
+
+- (void)setValue:(id)value forKey:(NSString *)key {
+    ldb.safe = YES;
+    ldb[key] = value;
+    ldb.safe = NO;
+}
+
+- (id)valueForKey:(NSString *)key {
+    return ldb[key];
+}
+
+- (void)addValue:(id)value forArrayKey:(NSString *)arrayKey {
+    NSMutableArray *array = [ldb[arrayKey] mutableCopy];
+    ldb.safe = YES;
+    if ([value isKindOfClass:[NSArray class]]) {
+        [array addObjectsFromArray:value];
+    } else {
+        [array addObject:value];
+    }
+    ldb.safe = NO;
+}
+
+- (NSArray *)valueForArrayKey:(NSString *)arrayKey start:(NSUInteger)start count:(NSUInteger)count {
+    NSArray *array = ldb[arrayKey];
+    if (start + count <= array.count) {
+        NSRange range = NSMakeRange(array.count - start - count, count);
+        NSArray *value = [array subarrayWithRange:range];
+        return value;
+    } else {
+        return @[];
     }
 }
 
